bb43891c6b0c16822390234f892dc167ed99764e
Ju Zhang <jzha263@aucklanduni.ac.nz>
2016-09-03 11:05:46 +1200

    added license, requirements, setup.py. v1.0.0 release

--- mapclientplugins/fieldworkmodelserialiserstep/__init__.py
+++ mapclientplugins/fieldworkmodelserialiserstep/__init__.py
@@ -17,9 +17,9 @@ This file is part of MAP Client. (http://launchpad.net/mapclient)
     You should have received a copy of the GNU General Public License
     along with MAP Client.  If not, see <http://www.gnu.org/licenses/>..
 """
-__version__ = '0.1.0'
+__version__ = '1.0.0'
 __author__ = 'Ju Zhang'
 __stepname__ = 'Fieldwork Model Serialiser'
-__location__ = 'https://github.com/mapclient-plugins/fieldworkmodelserialiserstep/archive/master.zip'
+__location__ = 'https://github.com/mapclient-plugins/fieldworkmodelserialiserstep/archive/v1.0.0.zip'
 
 from mapclientplugins.fieldworkmodelserialiserstep import step
